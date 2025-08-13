4a5b6071710aa88315256b3881b8674ee5c1115d
antonio86itna <48077495+antonio86itna@users.noreply.github.com>
2025-08-13 17:23:53 +0200

    Update Plugin.php

--- src/Elementor/Plugin.php
+++ src/Elementor/Plugin.php
@@ -10,6 +10,15 @@ class Plugin {
     }
     public static function register_widgets($widgets_manager){
         require_once __DIR__ . '/Widgets/Search.php';
+        require_once __DIR__ . '/Widgets/Results.php';
+        require_once __DIR__ . '/Widgets/Checkout.php';
+        require_once __DIR__ . '/Widgets/Dashboard.php';
+        require_once __DIR__ . '/Widgets/Tariffe.php';
+        
         $widgets_manager->register( new \AMCB\Elementor\Widgets\Search() );
+        $widgets_manager->register( new \AMCB\Elementor\Widgets\Results() );
+        $widgets_manager->register( new \AMCB\Elementor\Widgets\Checkout() );
+        $widgets_manager->register( new \AMCB\Elementor\Widgets\Dashboard() );
+        $widgets_manager->register( new \AMCB\Elementor\Widgets\Tariffe() );
     }
 }
